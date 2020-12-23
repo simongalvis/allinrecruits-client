9fd3f54862665ad4e43bfb7f104cd75e61ffbb9f
Simon Galvis <simongalvis9@gmail.com>
2020-12-23 01:40:55 -0700

    Write smoke test and snapshot test for App

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import renderer from 'react-test-renderer';
+import {BrowserRouter} from 'react-router-dom'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe(`App component`, () =>{
+    it(`renders without crashing`, () =>{
+        const div = document.createElement('div');
+
+        ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, div)
+
+        ReactDOM.unmountComponentAtNode(div);
+    })
+    it(`Renders UI as expected`, () =>{
+        const tree = renderer
+            .create(<BrowserRouter><App/></BrowserRouter>)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    })
+})
